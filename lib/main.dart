cbe395a1ff0d46d130d55746b085d8f85f4f42bf
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-07-08 18:13:13 +0545

    optional variable and nullsafety

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+String? someValue;
 void main() {
-  final someValue1 = DateTime.now();
-  const someValuee2 = '12';
-  print(someValue1);
-  print(someValuee2);
+  
+  // ? = optional (hunu sakxau hai)
+  // ! = is not 
+  
+  print(someValue);
+  
+  
+  someValue  = null;
+  print(someValue?.length??0);    /* if some value null bhaney null parint gar
+                                  if null haina bhaney then use . length */
+
+  someValue = "hello my brother";
+  print (someValue?.length??1);
 }
