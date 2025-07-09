e3632b880b4e120644891c17b47e647bdbe47a4f
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-07-09 14:03:04 +0545

    if else >=

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
-String? someValue;
-void main() {
-  
-  // ? = optional (hunu sakxau hai)
-  // ! = is not 
-  
-  print(someValue);
-  
-  
-  someValue  = null;
-  print(someValue?.length??0);    /* if some value null bhaney null parint gar
-                                  if null haina bhaney then use . length */
 
-  someValue = "hello my brother";
-  print (someValue?.length??1);
+bool isAdult = false;
+
+void main() {
+  int age = 12;
+  if (isAdult = false) {
+    print("Adult 21");
+  } else if (age >= 18) {
+    print("Adult");
+  } else {
+    print("Child");
+  }
 }
