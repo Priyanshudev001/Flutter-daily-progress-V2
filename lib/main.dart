a5d8b09948d3d8716d856718ad8db8880bfc5562
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-07-15 13:18:39 +0545

    private variable in getter value

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 void main() {
   final cookie = Cookie(shape: 'Hello', size: 20);
+
+  print(cookie._height);
 }
+
 class Cookie {
   final String shape;
   final double size;
@@ -8,17 +11,31 @@ class Cookie {
   Cookie({required this.shape, required this.size}) {
     baking();
   }
+
+  // Private variable
   int _height = 4;
   int _width = 5;
+
+  // Getters
+  int get height => _height;
+  // Setters
+  
+
+  // method
+void modifiyHeight (int h){
+
+}
 
   int calculateSize() {
     return _height * _width;
   }
 
+  
   void baking() {
     print('hello mf k xa ?');
   }
 }
+
 bool isCooling() {
   return false;
 }
