bd99d43f112a1446af87c0a62de0f2395a5364ec
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-07-15 16:08:08 +0545

    static

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,15 @@
 void main() {
-  final cookie = Cookie(shape: 'Hello', size: 20);
-
-  print(cookie._height);
+  print(Constants.greeting);
+  print(Constants.bye);
+  print(Constants.giveMeSomeValue());
 }
 
-class Cookie {
-  final String shape;
-  final double size;
+class Constants {
+  static int height = 10;
+  static String greeting = "Hello are you ?";
+  static String bye = "Bye";
 
-  Cookie({required this.shape, required this.size}) {
-    baking();
+  static int giveMeSomeValue() {
+    return height;
   }
-
-  // Private variable
-  int _height = 4;
-  int _width = 5;
-
-  // Getters
-  int get height => _height;
-  // Setters
-  
-
-  // method
-void modifiyHeight (int h){
-
-}
-
-  int calculateSize() {
-    return _height * _width;
-  }
-
-  
-  void baking() {
-    print('hello mf k xa ?');
-  }
-}
-
-bool isCooling() {
-  return false;
 }
