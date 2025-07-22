e34e874cd1ddfe4eb104ee7fb47432a8ff608d96
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-07-22 15:00:42 +0545

    map basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 void main() {
-  // List
-  // [10, 20, 30]
-  final romanStudent = Student("Roman", 40);
-  Set<Student> students = {
-    Student("Priyanshu", 10),
-    Student("Messi", 20),
-    Student("Ronaldo", 30),
-    Student("Mbape", 40), romanStudent,romanStudent,romanStudent
-  };
+  // Map
 
-  print(students);
-}
+  final list = [10, 20, 30];
+
+  Map<String, int> marks = {'Priyanshu': 10, 'cat': 20, 'tata': 30};
+
+  if (marks['pagal']?.isEven == null) {
+    print('Key does not exist');
+  }
+    else{
+      print(marks['pagal']!.isEven);
+    
+    }
+  }
+
 
 class Student {
   final String name;
