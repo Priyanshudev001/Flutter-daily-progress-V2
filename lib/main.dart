70aad4ad6d04f726c1653b0b494a88d0a43ec2ee
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-07-27 12:15:04 +0545

    Iterating

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
 void main() {
-  // Map
+  // A list of student marks as maps
+  List<Map<String, int>> marks = [
+    {
+      'Math': 10,
+      'Computer': 30,
+      'Science': 20,
+    },
+    {
+      'Math': 10,
+      'Computer': 30,
+      'Science': 20,
+    },
+    {
+      "Math": 10,
+      "Science": 20,
+      "Computer": 30,
+    },
+  ];
 
-  final list = [10, 20, 30];
-
-  Map<int, String> marks = {10: "10", 20: "20", 30: "30"};
-
-  final anotherMap = {23: "22", 12: "3", 122: "hi"};
-  marks.addAll(anotherMap);
-  marks.remove(122);
-  print(marks);
+  // Iterating over each map in the list
+  marks.map((e) {
+    print(e);
+  }).toList();
 }
 
 class Student {
   final String name;
   final int marks;
+
   Student(this.name, this.marks);
+
   @override
   String toString() => 'Student : $name';
 }
