2809b95ea3f9b7197d3406eba8bdcecb05e3669c
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-07-29 14:41:57 +0545

    Exception Handeling

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,11 @@
 void main() {
-  //Enums
+  print(10 ~/ 3);
+  print(10 / 0);
 
-  final employee1 = Employee("Priyanshu", EmployeeType.doctor);
-  final employee2 = Employee("Priyanshu", EmployeeType.finance);
-
-  final employee3 = Employee("Sonal", EmployeeType.marketing);
-  employee1.fn();
-  employee2.fn();
-  employee3.fn();
-}
-
-enum EmployeeType {
-  doctor(122),
-  finance(213),
-  marketing(213);
-
-  final int salary;
-  const EmployeeType(this.salary);
-}
-
-class Employee {
-  final String name;
-  final EmployeeType type;
-
-  Employee(this.name, this.type);
-
-  void fn() {
-    print("${type.name} ${type.salary}");
+  try {
+    print(10 ~/ 0);
+  } catch (e) {
+    print('some error occurered');
   }
+  print("Hello brother");
 }
